f484da5d2c82cea7807eeeabbff421d9f7ffd1eb
Yotanwa <pl.audiger@laposte.net>
2023-04-06 21:34:32 +0200

    Update TodoListController.java

--- todolist-debut-ex2/src/main/java/com/acme/todolist/adapters/rest_api/TodoListController.java
+++ todolist-debut-ex2/src/main/java/com/acme/todolist/adapters/rest_api/TodoListController.java
@@ -4,11 +4,16 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.acme.todolist.application.port.in.GetTodoItems;
+import com.acme.todolist.application.port.in.AddTodoItem;
+import com.acme.todolist.application.service.AddTodoItemService;
 import com.acme.todolist.domain.TodoItem;
 
 /**
@@ -22,13 +27,15 @@ public class TodoListController {
 	
 	
 	private GetTodoItems getTodoItemsQuery;
-	// A compléter
+	
+	private AddTodoItemService addTodoItemsQuery;
 	
 	
 	@Inject
-	//A compléter
-	public TodoListController(GetTodoItems getTodoItemsQuery ) {
+	
+	public TodoListController(GetTodoItems getTodoItemsQuery, AddTodoItemService addTodoItemsQuery) {
 		this.getTodoItemsQuery = getTodoItemsQuery;
+		this.addTodoItemsQuery = addTodoItemsQuery;
 	}
 	
 	@GetMapping("/todos")
@@ -39,8 +46,10 @@ public class TodoListController {
 	
 	// Endpoint de type POST vers "/todos"
 	// A compléter
+	@PostMapping("/todos")
+	@ResponseStatus(code = HttpStatus.CREATED)
 	public void ajouterItem(@RequestBody TodoItem item) {
-		// A compléter		
+		this.addTodoItemsQuery.addTodoItem(item);
 	}
 	
 	
